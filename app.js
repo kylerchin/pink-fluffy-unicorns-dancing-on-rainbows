8d74f195383a3847d605c52a7478598a1117d97c
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-11-06 07:08:38 -0800

    add link command

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ client.on("message", async msg => {
     msg.channel.send("Pink fluffy unicorns dancing on dancing on rain");
   }
 
-  if(command === "video" || command === "v" || command === "music") {
+  if(command === "video" || command === "v" || command === "music" || command === "link") {
     msg.channel.send("https://youtu.be/eWM2joNb9NE");
   }
 
