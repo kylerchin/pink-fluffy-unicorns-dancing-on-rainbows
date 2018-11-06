7d7c38ea635c6fea7b1bed4fa723ae1dda1451f1
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2018-11-05 19:15:28 -0800

    print out all messages

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ client.on("guildDelete", guild => {
 client.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
 
+  console.log(message.content);
+
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
